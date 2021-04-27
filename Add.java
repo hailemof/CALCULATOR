cb5ea4f2b2c6a33d4a5f2bf4cb68709ca96e09f1
yosephdirirsa <yosephdirirsa@gmail.com>
2021-04-27 12:30:15 +0300

    this will add

---
+++ Add.java
@@ -0,0 +1,13 @@
+public class Add{
+
+private int firstNum;
+private int secondNum;
+
+public int adder(int firstNum, int secondNum){
+this.firstNum=firstNum;
+this.secondNum=secondNum;
+
+return firstNum+secondNum;
+
+
+}
